503bab06fae89d8189a3ce51ccbadaff0a0ad6f7
victorftsilva <victor.silva@grinply.com>
2021-12-22 14:43:04 -0300

    class that encapsulates a contract

--- autocrypto/contract.py
+++ autocrypto/contract.py
@@ -1,6 +1,22 @@
 from brownie.project import compiler
 import os
 
+class Contract:
+    def __init__(self, compiled_data: dict):
+        self.name = compiled_data['contractName']
+        self.abi = compiled_data['abi']
+        self.bytecode = compiled_data['deployedBytecode']
+        self.bytecode_sha1 = compiled_data['bytecodeSha1']
+        self.sources_paths = compiled_data['allSourcePaths']
+        self.address = None
+        self.web3 = None
+    
+    def deploy(self, args):
+        return self.web3.eth.contract(abi=self.abi, bytecode=self.bytecode).deploy(args)
+    
+    def call(self, args):
+        return self.web3.eth.contract(address=self.address, abi=self.abi).call(args)
+
 def get_contracts_path(root_path: str) -> list:
     contracts = []
     print(f"Compiling contracts on {root_path}/...")
@@ -15,13 +31,14 @@ def contract_code(contract_path: str) -> str:
     with open(contract_path, "r") as f:
         return f.read()
     
-def compile_autocrypto():
+def compile_autocrypto() -> dict:
     """compile autocrypto using all solidity files in the contracts directory"""
     contracts_path = get_contracts_path("../contracts")
     contract_data = {path : contract_code(path) for path in contracts_path}
     return compiler.compile_and_format(contract_sources=contract_data)
 
 if __name__ == "__main__":
-    compiled_data = compile_autocrypto()
-
-
+    compiled_data = compile_autocrypto()['AutoCrypto']
+    contract = Contract(compiled_data)
+    
+    
--- contracts/AutoCrypto.sol
+++ contracts/AutoCrypto.sol
@@ -1,2 +1,28 @@
 // SPDX-License-Identifier: GNU GPL-3.0-or-later
 pragma solidity 0.8.10;
+
+import "./libraries/SimplePair.sol";
+import "./libraries/SimpleOwner.sol";
+import "./interfaces/IPancakePair.sol";
+import "./interfaces/IPancakeFactory.sol";
+
+contract AutoCrypto is SimpleOwner{
+    using SimplePair for IPancakePair;
+
+    IPancakeFactory private constant PANCAKE_FACTORY = IPancakeFactory(0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73);
+
+    //returns the address of the pair (if it exists) on pancakeswap for the provided tokens 
+    function getPair(address token0, address token1) external view returns (address pair) {
+        require(token0 != token1,"token0 != token1");
+        pair = PANCAKE_FACTORY.getPair(token0, token1);
+        require (pair != address(0), "pair not found");
+    }
+    
+    // swaps two tokens in the provided pair
+    function swap(address _pair, address inToken, uint amount) external onlyOwner returns (uint amountOut) {
+        require(_pair != address(0), "!pair");
+        IPancakePair pair = IPancakePair(_pair);
+        require(pair.isOnPair(inToken), "!isOnPair");
+        amountOut = pair.simpleSwap(inToken, amount);
+    }
+}
