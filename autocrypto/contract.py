a43ad1d2450c4ee64deb0321a798805dcffc0af2
victorftsilva <victor.silva@grinply.com>
2021-12-22 19:25:03 -0300

    adding rpc url to the configuration

--- autocrypto/contract.py
+++ autocrypto/contract.py
@@ -1,23 +1,45 @@
 from brownie.project import compiler
+from web3.contract import Contract
+from web3 import Web3
 import os
 
-class Contract:
-    def __init__(self, compiled_data: dict):
-        self.name = compiled_data['contractName']
-        self.abi = compiled_data['abi']
-        self.bytecode = compiled_data['deployedBytecode']
-        self.bytecode_sha1 = compiled_data['bytecodeSha1']
-        self.sources_paths = compiled_data['allSourcePaths']
-        self.address = None
-        self.web3 = None
-    
-    def deploy(self, args):
-        return self.web3.eth.contract(abi=self.abi, bytecode=self.bytecode).deploy(args)
-    
-    def call(self, args):
-        return self.web3.eth.contract(address=self.address, abi=self.abi).call(args)
+class AutoCryptoContract(Contract):
+    def __init__(self, bytecode, abi, web3Conn: Web3, address: str = None):
+        if web3Conn is None or web3Conn.isConnected() is False:
+            raise ValueError("a working web3 connection is required")
+        if address is None and (bytecode is None or abi is None):
+            raise ValueError("a address or bytecode and abi are required")
+        if not address:
+            address = self._deploy(web3Conn, bytecode, abi) 
+        self.web3 = web3Conn
+        self.abi = abi
+        self.bytecode = bytecode 
+        super(AutoCryptoContract, self).__init__(address)
+        
+    def _deploy(self, web3, bytecode, abi) -> str:
+        """Deploy te contract to the blockchain (if it is not already deployed)"""
+        if self.address:
+            return self.address
+        contract = web3.eth.contract(abi=abi, bytecode=bytecode)
+        tx_hash = contract.constructor().transact()
+        return web3.eth.wait_for_transaction_receipt(tx_hash).contractAddress
 
-def get_contracts_path(root_path: str) -> list:
+def autocrypto_contract() -> Contract:
+    """return a instance of the Contract class containing the compiled autocrypto contract data"""
+    web3 = Web3(Web3.HTTPProvider("http://localhost:8545")) #TODO: take the rpc url from the config file
+    web3.eth.defaultAccount = web3.eth.accounts[0] #TODO: get the encrypted private key from the config file
+    
+    compiled_data = _compile_autocrypto()['AutoCrypto']
+    return AutoCryptoContract(compiled_data['deployedBytecode'], compiled_data['abi'], web3)
+
+def _compile_autocrypto() -> dict:
+    """compile autocrypto using all solidity files in the contracts directory"""
+    contracts_path = _get_contracts_path("../contracts")
+    contract_data = {path : _contract_code(path) for path in contracts_path}
+    return compiler.compile_and_format(contract_sources=contract_data)
+
+def _get_contracts_path(root_path: str) -> list:
+    """return a list of solidity files in the root_path directory provided"""
     contracts = []
     print(f"Compiling contracts on {root_path}/...")
     for root, _, files in os.walk(root_path):
@@ -27,18 +49,12 @@ def get_contracts_path(root_path: str) -> list:
                 contracts.append(os.path.join(root, file))
     return contracts
 
-def contract_code(contract_path: str) -> str:
+def _contract_code(contract_path: str) -> str:
+    """return the contract code from a solidity file"""
     with open(contract_path, "r") as f:
         return f.read()
-    
-def compile_autocrypto() -> dict:
-    """compile autocrypto using all solidity files in the contracts directory"""
-    contracts_path = get_contracts_path("../contracts")
-    contract_data = {path : contract_code(path) for path in contracts_path}
-    return compiler.compile_and_format(contract_sources=contract_data)
 
 if __name__ == "__main__":
-    compiled_data = compile_autocrypto()['AutoCrypto']
-    contract = Contract(compiled_data)
+    autocrypto = autocrypto_contract()
     
     
--- contracts/libraries/SimplePair.sol
+++ contracts/libraries/SimplePair.sol
@@ -9,9 +9,9 @@ library SimplePair {
     IWBNB constant WBNB = IWBNB(0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c);
     address private constant PANCAKE_FACTORY = 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
 
-    // executes a simple swap of tokens in this pool.
+    // executes a simple swap of tokens in this pair.
     // @amountIn is the amount you want to spend on the swap
-    // @token is the address of one of the tokens in the pull that is associated with the @amountIn
+    // @token is the address of one of the tokens in the pair, this token is the associated with the @amountIn
     // note: no previous checks are required, you only need to input a @amountIn that is avaliable and the token address that is in the pair
     // output amount and other calculations are handled by the function internally
     function simpleSwap(IPancakePair pair, address token, uint amountIn) internal returns (uint) {
@@ -31,7 +31,7 @@ library SimplePair {
             ? (reserve0, reserve1)
             : (reserve1, reserve0);
 
-        uint amountOut = getAmountOut(pair.factory(), amountIn, reserveA, reserveB);
+        uint amountOut = getAmountOut(amountIn, reserveA, reserveB);
 
         (uint256 amount0Out, uint256 amount1Out) = 
             token == pair.token0()
@@ -44,63 +44,16 @@ library SimplePair {
         return amountOut;
     }
 
-    //burn all LP tokens in the given pair
-    function burnLPTokens(IPancakePair pair) internal{
-        address myaddr = address(this);
-        require(pair.balanceOf(myaddr) > 0, "!nolp");
-        pair.transfer(address(pair), pair.balanceOf(myaddr));
-        pair.burn(myaddr);
-    }
-
-    //mint LP tokens for the @pair with @amountInToken of the @token
-    function mintLPTokens(IPancakePair pair, address token, uint amountInToken) internal returns (uint lpTokens){
-        if(token == address(WBNB)){
-            depositWBNB(amountInToken);
-        } 
-
-        IERC20 tokenA = IERC20(token);
-        require(IERC20(token).balanceOf(address(this)) >= amountInToken, "!balance_a"); //checking if we have the amount needed of tokens a to mint
-
-        (uint reserve0, uint reserve1,) = pair.getReserves();
-        (uint reserveA, uint reserveB) = 
-            token == pair.token0()
-            ? (reserve0, reserve1)
-            : (reserve1, reserve0);
-
-
-        uint amountTokenB = amountLiquidity(amountInToken, reserveA, reserveB);
-        
-        if(pair.token0() == address(WBNB) || pair.token1() == address(WBNB)){
-            uint amountBNB = token == address(WBNB) ? amountInToken : amountTokenB;
-            depositWBNB(amountBNB);
-        } 
-
-        IERC20 tokenB = IERC20(pair.token0() == token ? pair.token1() : pair.token0());
-        require(tokenB.balanceOf(address(this)) >= amountTokenB, "!balance_b"); //checking if we have the amount needed of tokens b to mint
-        
-        //sending the tokens to the pair contract for minting
-        tokenA.transfer(address(pair), amountInToken);
-        tokenB.transfer(address(pair), amountTokenB);
-
-        lpTokens = pair.mint(address(this)); //mint tokens to this contract address
-    }
-
     // given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
-    function getAmountOut(address factory, uint amountIn, uint reserveIn, uint reserveOut) internal returns (uint amountOut) {
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
         require(amountIn > 0, 'INSUFFICIENT_INPUT_AMOUNT');
         require(reserveIn > 0 && reserveOut > 0, 'INSUFFICIENT_LIQUIDITY');
 
-        (uint m, uint n) = (1000,2);
+        (uint m, uint n) = (1000,2); //m and n are the constants in the PancakePair contract for pancakeswap pairs
         uint amountInWithFee = amountIn * (m - n);
         uint numerator = amountInWithFee * reserveOut;
         uint denominator = reserveIn * m + amountInWithFee;
         amountOut = numerator / denominator;
-    }
-    
-    // calculates the amount of tokens that you need to deposit to mint LP tokens given a deposit quantity of @amountIn
-    // and the current reserve value of each token in the pair
-    function amountLiquidity(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
-        amountOut = amountIn * reserveOut / reserveIn;
     }
     
     //convert enough bnb into wbnb to reach @amountIn in wbnb ( only if the current amount of wbnb is less than @amountIn )
@@ -115,17 +68,14 @@ library SimplePair {
         WBNB.deposit{value:differenceNeeded}();//convert bnb to wbnb 
     }
 
+    //checks if the @token is present in the @pair
     function isOnPair(IPancakePair pair, address token) internal view returns (bool) {
         return pair.token0() == token || pair.token1() == token;
     }
 
-    function isToken0(IPancakePair pair, address token) internal view returns (bool) {
-        return token == pair.token0();
-    }
-
-    function reserve(IPancakePair pair, address token) internal returns (uint) {
+    //returns the reserves for the @token in the @pair
+    function reserve(IPancakePair pair, address token) internal view returns (uint) {
         (uint reserve0, uint reserve1,) = pair.getReserves();
         return token == pair.token0() ?reserve0 : reserve1;
     }
-
 }
